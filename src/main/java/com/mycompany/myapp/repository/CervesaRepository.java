f01fc45a102e1eae2118c0849f3caf291a69a5e9
Víctor <victorcorreasvillas@gmail.com>
2016-05-23 16:35:50 +0200

    Intentando acabar top 10

--- src/main/java/com/mycompany/myapp/repository/CervesaRepository.java
+++ src/main/java/com/mycompany/myapp/repository/CervesaRepository.java
@@ -1,6 +1,7 @@
 package com.mycompany.myapp.repository;
 
 import com.mycompany.myapp.domain.Cervesa;
+import com.mycompany.myapp.domain.Evaluar;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.*;
@@ -17,7 +18,9 @@ public interface CervesaRepository extends JpaRepository<Cervesa,Long> {
     /*@Query("select c from Cervesa c  order by c.evaluars desc ")// : objeto param NO VA
     List<Cervesa> TopCervezas();*/
     //@Query("select c from Cervesa c group by c.evaluars order by COUNT(c.evaluars) desc ")
+
     @Query("select c from Cervesa c ") //este funciona
+    //@Query("select c from Cervesa c, Evaluar ev where c.evaluars = ev.id")
     Page<Cervesa> findByTopCervesas(Pageable topTen);
 
     //@Query("select c from Cervesa c group by c.evaluars order by COUNT(c.evaluars) desc ")
--- src/main/webapp/top.controller.js
+++ src/main/webapp/top.controller.js
@@ -1,13 +1,13 @@
-(function() {
+/*(function() {
     'use strict';
 
     angular
         .module('jhipsterApp')
         .controller('CtrlTop', TOPController);
 
-    TOPController.$inject = ['$scope', 'Principal', 'LoginService', 'Cervesa',];
+    TOPController.$inject = ['$scope', 'Principal', 'LoginService', 'Cervesa', '$http'];
 
-    function TOPController ($scope, Principal, Cervesa, LoginService) {
+    function TOPController ($scope, Principal, LoginService, Cervesa, $http) {
         var vm = this;
 
         vm.account = null;
@@ -32,6 +32,18 @@
             Cervesa.consultaTop( function (response){
             //$http.get("api/cervesas").then(function (response) {
                 $scope.cervesas = response;
+                //});
         })
-    }}});
+    }
+    }
+});
+*/
+ 'use strict'
+ angular.module('jhipsterApp')
+ .controller('CtrlTop',function ($scope, $http, Cervesa) {
 
+         Cervesa.consultaTop(function (response) {
+             $scope.cervesas = response;
+         })
+
+ });
